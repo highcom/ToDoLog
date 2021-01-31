13228a59503935d405f665e01d49310c5b6ba93d
koichi highcom <youhappywith@yahoo.co.jp>
2021-01-31 23:32:48 +0900

    DrawerLayoutにListViewを追加してFragmentを制御する修正

--- app/src/main/java/com/highcom/todolog/MainActivity.java
+++ app/src/main/java/com/highcom/todolog/MainActivity.java
@@ -1,24 +1,31 @@
 package com.highcom.todolog;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+import com.highcom.todolog.ui.gallery.GalleryFragment;
+import com.highcom.todolog.ui.home.HomeFragment;
+import com.highcom.todolog.ui.slideshow.SlideshowFragment;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration mAppBarConfiguration;
+    private static final String lists[] = { "TASK1", "TASK2", "TASK3"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,16 +42,44 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle actionBarDrawerToggle =
+                new ActionBarDrawerToggle(
+                        this, drawer, toolbar, R.string.drawer_open, R.string.drawer_close);
+        drawer.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
         NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                return false;
+            }
+        });
+
+        ListView listView = findViewById(R.id.task_list_view_inside_nav);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, lists);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                switch (i) {
+                    case 0:
+                        Fragment galleryFragment = new GalleryFragment();
+                        fragmentManager.beginTransaction().replace(R.id.nav_host_fragment, galleryFragment).commit();
+                        break;
+                    case 1:
+                        Fragment homeFragment = new HomeFragment();
+                        fragmentManager.beginTransaction().replace(R.id.nav_host_fragment, homeFragment).commit();
+                        break;
+                    case 2:
+                        Fragment slideshowFragment = new SlideshowFragment();
+                        fragmentManager.beginTransaction().replace(R.id.nav_host_fragment, slideshowFragment).commit();
+                        break;
+                }
+                drawer.closeDrawers();
+            }
+        });
     }
 
     @Override
@@ -52,12 +87,5 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
     }
 }
