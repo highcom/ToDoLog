c9b3b385f5218c8840c550c3dcc0054cc837e1a4
koichi highcom <youhappywith@yahoo.co.jp>
2021-02-09 18:10:03 +0900

    AdapterをSwipeしてスライドボタンを表示する実装

--- app/src/main/java/com/highcom/todolog/ui/todolist/ToDoListFragment.java
+++ app/src/main/java/com/highcom/todolog/ui/todolist/ToDoListFragment.java
@@ -1,5 +1,7 @@
 package com.highcom.todolog.ui.todolist;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,13 +16,19 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.highcom.todolog.R;
+import com.highcom.todolog.ToDoDetailActivity;
 import com.highcom.todolog.datamodel.ToDoViewModel;
+import com.highcom.todolog.ui.DividerItemDecoration;
+import com.highcom.todolog.ui.SimpleCallbackHelper;
 
-public class ToDoListFragment extends Fragment {
+import java.util.List;
+
+public class ToDoListFragment extends Fragment implements SimpleCallbackHelper.SimpleCallbackListener{
 
     private String mSelectGroup;
     private LifecycleOwner mOwner;
     private ToDoViewModel mToDoViewModel;
+    private SimpleCallbackHelper simpleCallbackHelper;
 
     public ToDoListFragment(LifecycleOwner owner) {
         this.mOwner = owner;
@@ -43,10 +51,50 @@ public class ToDoListFragment extends Fragment {
         ToDoListAdapter adapter = new ToDoListAdapter(new ToDoListAdapter.ToDoDiff());
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        // セル間に区切り線を実装する
+        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL_LIST);
+        recyclerView.addItemDecoration(itemDecoration);
 
         mToDoViewModel = new ViewModelProvider(this).get(ToDoViewModel.class);
         mToDoViewModel.getToDoListByTaskGroup(mSelectGroup).observe(this.mOwner, todolist -> {
             adapter.submitList(todolist);
         });
+
+        final float scale = getResources().getDisplayMetrics().density;
+        // ドラックアンドドロップの操作を実装する
+        simpleCallbackHelper = new SimpleCallbackHelper(getContext(), recyclerView, scale, this) {
+            @Override
+            public void instantiateUnderlayButton(RecyclerView.ViewHolder viewHolder, List<UnderlayButton> underlayButtons) {
+                underlayButtons.add(new SimpleCallbackHelper.UnderlayButton(
+                        "削除",
+                        0,
+                        Color.parseColor("#FF3C30"),
+                        (RecyclerView.ViewHolder) viewHolder,
+                        (holder, pos) -> {
+                            // 削除処理を実装する
+                        }
+                ));
+                underlayButtons.add(new SimpleCallbackHelper.UnderlayButton(
+                        "詳細",
+                        0,
+                        Color.parseColor("#C7C7CB"),
+                        (RecyclerView.ViewHolder) viewHolder,
+                        (holder, pos) -> {
+                            Intent intent = new Intent(getContext(), ToDoDetailActivity.class);
+                            startActivity(intent);
+                        }
+                ));
+            }
+        };
+    }
+
+    @Override
+    public boolean onSimpleCallbackMove(RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+        return false;
+    }
+
+    @Override
+    public void clearSimpleCallbackView(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder) {
+
     }
 }
