c9e69a40773646977ce4fec79e2dbab3df713b49
koichi highcom <youhappywith@yahoo.co.jp>
2021-03-17 22:14:49 +0900

    初期起動時のデータを設定

--- app/src/main/java/com/highcom/todolog/datamodel/ToDoLogRoomDatabase.java
+++ app/src/main/java/com/highcom/todolog/datamodel/ToDoLogRoomDatabase.java
@@ -9,6 +9,8 @@ import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
+import com.highcom.todolog.R;
+
 import java.sql.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -21,6 +23,7 @@ abstract class ToDoLogRoomDatabase extends RoomDatabase {
     abstract LogDao logDao();
 
     private static volatile ToDoLogRoomDatabase INSTANCE;
+    private static Context mContext;
     private static final int NUMBER_OF_THREADS = 4;
     static final ExecutorService databaseWriteExtractor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
 
@@ -28,6 +31,7 @@ abstract class ToDoLogRoomDatabase extends RoomDatabase {
         if (INSTANCE == null) {
             synchronized (ToDoLogRoomDatabase.class) {
                 if (INSTANCE == null) {
+                    mContext = context;
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(), ToDoLogRoomDatabase.class, "todolog_database")
                             .addCallback(sRoomDatabaseCallback)
                             .build();
@@ -45,33 +49,51 @@ abstract class ToDoLogRoomDatabase extends RoomDatabase {
             databaseWriteExtractor.execute(() -> {
                 GroupDao groupDao = INSTANCE.groupDao();
                 groupDao.deleteAll();
-                groupDao.insert(new Group(1, 2, "GROUP1"));
-                groupDao.insert(new Group(2, 3, "GROUP2"));
-                groupDao.insert(new Group(3, 4, "GROUP3"));
-                groupDao.insert(new Group(4, 1, "GROUP4"));
+                groupDao.insert(new Group(1, 1, mContext.getString(R.string.group_data_1)));
+                groupDao.insert(new Group(2, 2, mContext.getString(R.string.group_data_2)));
+                groupDao.insert(new Group(3, 3, mContext.getString(R.string.group_data_3)));
 
                 LogDao logDao = INSTANCE.logDao();
                 logDao.deleteAll();
                 logDao.insert(new Log(1, 1, new Date(System.currentTimeMillis()), Log.LOG_CREATE_NEW));
-                logDao.insert(new Log(2, 1, new Date(System.currentTimeMillis()), Log.LOG_CHANGE_CONTENTS));
-                logDao.insert(new Log(7, 1, new Date(System.currentTimeMillis()), Log.LOG_CHANGE_CONTENTS));
-                logDao.insert(new Log(3, 2, new Date(System.currentTimeMillis()), Log.LOG_CREATE_NEW));
-                logDao.insert(new Log(4, 3, new Date(System.currentTimeMillis()), Log.LOG_CREATE_NEW));
-                logDao.insert(new Log(5, 4, new Date(System.currentTimeMillis()), Log.LOG_CREATE_NEW));
-                logDao.insert(new Log(6, 5, new Date(System.currentTimeMillis()), Log.LOG_CREATE_NEW));
-                logDao.insert(new Log(8, 6, new Date(System.currentTimeMillis()), Log.LOG_CREATE_NEW));
-                logDao.insert(new Log(9, 7, new Date(System.currentTimeMillis()), Log.LOG_CREATE_NEW));
+                logDao.insert(new Log(2, 2, new Date(System.currentTimeMillis()), Log.LOG_CREATE_NEW));
+                logDao.insert(new Log(3, 3, new Date(System.currentTimeMillis()), Log.LOG_CREATE_NEW));
+                logDao.insert(new Log(4, 4, new Date(System.currentTimeMillis()), Log.LOG_CREATE_NEW));
+                logDao.insert(new Log(5, 5, new Date(System.currentTimeMillis()), Log.LOG_CREATE_NEW));
+                logDao.insert(new Log(6, 6, new Date(System.currentTimeMillis()), Log.LOG_CREATE_NEW));
+                logDao.insert(new Log(7, 7, new Date(System.currentTimeMillis() - (1000 * 60 * 60 * 24)), Log.LOG_CREATE_NEW));
+                logDao.insert(new Log(8, 8, new Date(System.currentTimeMillis() - (1000 * 60 * 60 * 24)), Log.LOG_CREATE_NEW));
+                logDao.insert(new Log(9, 9, new Date(System.currentTimeMillis()), Log.LOG_CREATE_NEW));
+                logDao.insert(new Log(10, 10, new Date(System.currentTimeMillis()), Log.LOG_CREATE_NEW));
+                logDao.insert(new Log(11, 11, new Date(System.currentTimeMillis()), Log.LOG_CREATE_NEW));
+                logDao.insert(new Log(12, 7, new Date(System.currentTimeMillis()), Log.LOG_CHANGE_STATUS_DONE));
+                logDao.insert(new Log(13, 8, new Date(System.currentTimeMillis()), Log.LOG_CHANGE_STATUS_DONE));
+                logDao.insert(new Log(14, 12, new Date(System.currentTimeMillis()), Log.LOG_CREATE_NEW));
+                logDao.insert(new Log(15, 13, new Date(System.currentTimeMillis() - (1000 * 60 * 60 * 24 * 2)), Log.LOG_CREATE_NEW));
+                logDao.insert(new Log(16, 14, new Date(System.currentTimeMillis()), Log.LOG_CREATE_NEW));
+                logDao.insert(new Log(17, 15, new Date(System.currentTimeMillis()), Log.LOG_CREATE_NEW));
+                logDao.insert(new Log(18, 13, new Date(System.currentTimeMillis() - (1000 * 60 * 60 * 24)), Log.LOG_CHANGE_CONTENTS));
+                logDao.insert(new Log(19, 13, new Date(System.currentTimeMillis()), Log.LOG_CHANGE_STATUS_DONE));
 
                 ToDoDao todoDao = INSTANCE.toDoDao();
                 todoDao.deleteAll();
-                todoDao.insert(new ToDo(1, 1, 1, 1, "作業内容1", 7));
-                todoDao.insert(new ToDo(2, 2, 1, 1, "作業内容2", 3));
-                todoDao.insert(new ToDo(3, 1, 2, 1, "作業内容3", 4));
-                todoDao.insert(new ToDo(4, 1, 1, 2, "作業内容4", 5));
-                todoDao.insert(new ToDo(5, 1, 1, 3, "作業内容5", 6));
-                todoDao.insert(new ToDo(6, 2, 2, 1, "作業内容6", 8));
-                todoDao.insert(new ToDo(7, 1, 1, 4, "作業内容7", 9));
+                todoDao.insert(new ToDo(1, 1, 1, 1, mContext.getString(R.string.todo_data_1), 1));
+                todoDao.insert(new ToDo(2, 2, 1, 1, mContext.getString(R.string.todo_data_2), 2));
+                todoDao.insert(new ToDo(3, 3, 1, 1, mContext.getString(R.string.todo_data_3), 3));
+                todoDao.insert(new ToDo(4, 4, 1, 1, mContext.getString(R.string.todo_data_4), 4));
+                todoDao.insert(new ToDo(5, 5, 1, 1, mContext.getString(R.string.todo_data_5), 5));
+                todoDao.insert(new ToDo(6, 6, 1, 1, mContext.getString(R.string.todo_data_6), 6));
 
+                todoDao.insert(new ToDo(7, 1, 2, 2, mContext.getString(R.string.todo_data_7), 12));
+                todoDao.insert(new ToDo(8, 2, 2, 2, mContext.getString(R.string.todo_data_8), 13));
+                todoDao.insert(new ToDo(9, 3, 1, 2, mContext.getString(R.string.todo_data_9), 9));
+                todoDao.insert(new ToDo(10, 4, 1, 2, mContext.getString(R.string.todo_data_10), 10));
+                todoDao.insert(new ToDo(11, 5, 1, 2, mContext.getString(R.string.todo_data_11), 11));
+
+                todoDao.insert(new ToDo(12, 1, 1, 3, mContext.getString(R.string.todo_data_12), 14));
+                todoDao.insert(new ToDo(13, 1, 2, 3, mContext.getString(R.string.todo_data_13), 19));
+                todoDao.insert(new ToDo(14, 3, 1, 3, mContext.getString(R.string.todo_data_14), 16));
+                todoDao.insert(new ToDo(15, 4, 1, 3, mContext.getString(R.string.todo_data_15), 17));
             });
         }
     };
